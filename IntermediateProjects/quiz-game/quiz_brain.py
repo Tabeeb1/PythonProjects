f15f7a68b72b20752e6398e703cea6a657563c99
Tahmeed Tabeeb <tahmeed.tabeeb@g.bracu.ac.bd>
2022-12-16 06:47:57 -0800

    Create quiz_brain.py

---
+++ IntermediateProjects/quiz-game/quiz_brain.py
@@ -0,0 +1,26 @@
+class QuizBrain:
+    def __init__(self,question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def next_question(self):
+        user_answer = input(f'Q.{(self.question_number)+1}: {(self.question_list[self.question_number]).text} (True/False): ')
+        self.check_answer(user_answer,(self.question_list[self.question_number]).answer)
+        self.question_number += 1
+
+
+
+    def still_has_questions(self):
+        if self.question_number >= len(self.question_list):
+            return False
+        else:
+            return True
+
+    def check_answer(self,user_answer,correct_answer):
+        if user_answer == correct_answer:
+            self.score += 1
+            print(f'Your answer was correct! Your current score is {self.score}/{self.question_number + 1}\n')
+
+        else:
+            print(f'Your answer was wrong, correct answer is {correct_answer}\n')
